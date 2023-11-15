b39d901bbd0b3f5b8491add71696d93a550d1bb6
Raihan <raihanhn20@gmail.com>
2023-11-16 01:21:49 +0600

    register page added

---
+++ src/app/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="border-t p-8 text-center text-gray-500 mt-8">
+      &copy; 2023 All right reserved
+    </footer>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Header from "./components/layout/Header";
 import Hero from "./components/layout/Hero";
 import HomeMenu from "./components/layout/HomeMenu";
 import SectionHeaders from "./components/layout/SectionHeaders";
@@ -7,7 +6,6 @@ import SectionHeaders from "./components/layout/SectionHeaders";
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
       <HomeMenu />
       <section className="text-center my-16">
