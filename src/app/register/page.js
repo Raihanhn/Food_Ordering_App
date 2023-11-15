9e5eaf46bbcddab2f9fad102a07b5878e6cd68a5
Raihan <raihanhn20@gmail.com>
2023-11-16 04:30:47 +0600

    user registraion completed

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,36 +1,55 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { useState } from "react";
 
 export default function Registerpage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  function handleFormSubmit(ev) {
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  async function handleFormSubmit(ev) {
     ev.preventDefault();
-    fetch("/api/register", {
+    setCreatingUser(true);
+    await fetch("/api/register", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
     });
+    setCreatingUser(false);
+    setUserCreated(true);
   }
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
+      {userCreated && (
+        <div className="my-4 text-center">
+          User created.
+          <br /> Now you can{" "}
+          <Link className="underline" href={"/login"}>
+            Login &raquo;{" "}
+          </Link>
+        </div>
+      )}
       <form className="block max-w-xl mx-auto " onSubmit={handleFormSubmit}>
         <input
           type="email"
           placeholder="email"
           value={email}
           onChange={(ev) => setEmail(ev.target.value)}
+          disabled={creatingUser}
         />
         <input
           type="password"
           placeholder="password"
           value={password}
           onChange={(ev) => setPassword(ev.target.value)}
+          disabled={creatingUser}
         />
-        <button type="submit">Register</button>
+        <button type="submit" disabled={creatingUser}>
+          Register
+        </button>
         <div className="my-4 text-center text-gray-500">
           or login with provider
         </div>
@@ -38,6 +57,12 @@ export default function Registerpage() {
           <Image src={"/google.png"} alt="" width={24} height={24} />
           Login with google
         </button>
+        <div className="text-center my-4 text-gray-500 pt-4">
+          Exisiting account?{" "}
+          <Link className="underline" href={"/login"}>
+            Login here &raquo;
+          </Link>
+        </div>
       </form>
     </section>
   );
