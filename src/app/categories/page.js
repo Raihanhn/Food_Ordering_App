53262a025a6c0998f8e58e4914f7f036bcb25045
Raihan <raihanhn20@gmail.com>
2023-11-19 06:10:00 +0600

    create category functions

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -2,9 +2,29 @@
 import React, { useEffect, useState } from "react";
 import UserTabs from "../components/layout/UserTabs";
 import { useProfile } from "../components/UseProfile";
+import toast from "react-hot-toast";
 
 export default function Categories() {
+  const [newCategoryName, setNewCategoryName] = useState("");
   const { loading: profileLoading, data: profileData } = useProfile();
+
+  async function handleNewCategorySubmit(ev) {
+    ev.preventDefault();
+    const creationPromise = new Promise(async (resolve, reject) => {
+      const response = await fetch("/api/categories", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: newCategoryName }),
+      });
+      if (response.ok) resolve();
+      else reject();
+    });
+    await toast.promise(creationPromise, {
+      loading: "Creating your new category...",
+      success: "Category created",
+      error: "Error, sorry...",
+    });
+  }
 
   if (profileLoading) {
     return "Loading user info...";
@@ -15,9 +35,25 @@ export default function Categories() {
   }
 
   return (
-    <section className="mt-8 max-w-lg mx-auto">
+    <section className="mt-8 max-w-md mx-auto">
       <UserTabs isAdmin={true} />
-      Categories
+      <form onSubmit={handleNewCategorySubmit}>
+        <div className="flex gap-2 items-end">
+          <div className="grow">
+            <label>New category name</label>
+            <input
+              type="text"
+              value={newCategoryName}
+              onChange={(ev) => setNewCategoryName(ev.target.value)}
+            />
+          </div>
+          <div className="pb-2">
+            <button className="border border-primary" type="submit">
+              Create
+            </button>
+          </div>
+        </div>
+      </form>
     </section>
   );
 }
